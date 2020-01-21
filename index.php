49d3e9580a772fcd1302ffbdbb03bfc524fe8f6f
haunt.hcm2015 <haunt.hcm2015@gmail.com>
2020-01-22 05:42:24 +0700

    Interface Interactive Design

--- index.php
+++ index.php
@@ -15,11 +15,22 @@
         <link rel="stylesheet" href="<?php echo BASE_URL.'assets/css/style.css'; ?>" type="text/css" crossorigin="anonymous"/>
     </head>
     <body>
+    <div class="container-fluid">
         <?php include_once("{$baseDir}core{$ds}classes{$ds}header.php");?>
-
+        <div class="slide img-fluid">
+            <figure id="multiSlide" class="multiSlide"></figure>
+            <nav id="multiSlideNav" class="multiSlideNav">
+                <a href="#" id="top" class="navButton"><</a>
+                <a href="#" id="bottom" class="navButton">></a>
+                <a href="#" id="left" class="navButton"><</a>
+                <a href="#" id="right" class="navButton">></a>
+            </nav>
+        </div>  
         <?php include_once("{$baseDir}core{$ds}classes{$ds}footer.php");?>
+    </div>
     </body>
     <script src="<?php echo BASE_URL.'assets/js/jquery-3.2.1.slim.min.js'; ?>" crossorigin="anonymous"></script>
+    <script src="<?php echo BASE_URL.'assets/js/animate.js'; ?>" crossorigin="anonymous"></script>
     <script src="<?php echo BASE_URL.'assets/js/popper.min.js'; ?>" crossorigin="anonymous"></script>
     <script src="<?php echo BASE_URL.'assets/js/bootstrap.min.js'; ?>" crossorigin="anonymous"></script>
 </html> 
