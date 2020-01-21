449350101268001e80186fb764fe91e534c8335f
haunt.hcm2015 <haunt.hcm2015@gmail.com>
2020-01-21 17:21:39 +0700

    .

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
         <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
         <title>AI Solution - Interdisciplinary Research</title>
-        <link rel="stylesheet" href="<?php echo BASE_URL.'assets/css/bootstrap.min.css'; ?>" type="text/css"/>
-        <link rel="stylesheet" href="<?php echo BASE_URL.'assets/css/style.css'; ?>" type="text/css"/>
+        <link rel="stylesheet" href="<?php echo BASE_URL.'assets/css/bootstrap.min.css'; ?>" type="text/css" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="<?php echo BASE_URL.'assets/css/style.css'; ?>" type="text/css" crossorigin="anonymous"/>
     </head>
     <body>
         <?php include_once("{$baseDir}core{$ds}classes{$ds}header.php");?>
-        
+
         <?php include_once("{$baseDir}core{$ds}classes{$ds}footer.php");?>
     </body>
     <script src="<?php echo BASE_URL.'assets/js/jquery-3.2.1.slim.min.js'; ?>" crossorigin="anonymous"></script>
