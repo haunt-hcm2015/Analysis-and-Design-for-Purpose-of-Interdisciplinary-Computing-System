c035d5bd8a720240fab1021e09a7f67ab0253706
haunt.hcm2015 <haunt.hcm2015@gmail.com>
2020-01-22 07:07:25 +0700

    Interface Interactive Design

--- core/init.php
+++ core/init.php
@@ -6,4 +6,15 @@ if (!isset($_SESSION)){
     session_start();
 }
 define("BASE_URL", "http://localhost:81/ai-solution/");
+
+function execInBackground($cmd) {
+    if (substr(php_uname(), 0, 7) == "Windows"){
+        pclose(popen("start /B ". $cmd, "r")); 
+    }
+    else {
+        exec($cmd . " > /dev/null &");  
+    }
+}
+// $technology = 'python implementation_algorithm/manage.py runserver';
+// shell_exec($technology);
 ?>
